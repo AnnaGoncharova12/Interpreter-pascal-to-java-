357933d3d3f65c6febc95e90177582f7d1521447
AnnaGoncharova12 <78485605+AnnaGoncharova12@users.noreply.github.com>
2021-02-03 12:06:01 -0500

    Create ParseException.java

---
+++ ParseException.java
@@ -0,0 +1,12 @@
+package interpretor;
+
+public class ParseException extends Exception{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public String toString() {
+	return "Invalid Syntax";
+}
+}
