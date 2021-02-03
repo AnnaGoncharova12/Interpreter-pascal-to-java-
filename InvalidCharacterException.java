6fae2be1e5787fd15e726646466b61d8034e09bc
AnnaGoncharova12 <78485605+AnnaGoncharova12@users.noreply.github.com>
2021-02-03 12:03:51 -0500

    Create InvalidCharacterException.java

---
+++ InvalidCharacterException.java
@@ -0,0 +1,9 @@
+package interpretor;
+
+public class InvalidCharacterException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public String toString() {
+		return "invalid character";
+	}
+}
