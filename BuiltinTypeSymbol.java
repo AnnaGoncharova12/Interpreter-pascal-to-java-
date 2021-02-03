91b7b47e904aae2d6f6d57a3f2a98a26f0f07395
AnnaGoncharova12 <78485605+AnnaGoncharova12@users.noreply.github.com>
2021-02-03 12:02:30 -0500

    Create BuiltinTypeSymbol.java

---
+++ BuiltinTypeSymbol.java
@@ -0,0 +1,13 @@
+package interpretor;
+
+public class BuiltinTypeSymbol extends Symbol{
+   public BuiltinTypeSymbol(String name, Symbol type) {
+		super(name, null);
+		// TODO Auto-generated constructor stub
+	}
+
+public String toString() {
+	return this.name;
+}
+    
+}
