c7c9ea77611ff9d591ed3fc2fb371cd6491e13ee
AnnaGoncharova12 <78485605+AnnaGoncharova12@users.noreply.github.com>
2021-02-03 12:08:49 -0500

    Create Symbol.java

---
+++ Symbol.java
@@ -0,0 +1,11 @@
+package interpretor;
+
+public class Symbol {
+String name;
+Symbol type;
+	  public Symbol(String name, Symbol type) {
+	        this.name = name;
+	        
+	     this.type = type;
+	  }
+}
